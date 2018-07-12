745729a813ddde0de065b5e8ce3d6d404a853e7d
Duc Tho Tran <ducthotran2010@gmail.com>
2018-07-12 14:10:37 +0700

    [Admin][EquipmentManager] Add new controllers

---
+++ src/java/Tho/Controllers/Admin/EquipmentManager/UpdateController.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tho.Controllers.Admin.EquipmentManager;
+
+import Tho.Models.EquipmentDAO;
+import Tho.Models.EquipmentDTO;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author ThoDT
+ */
+public class UpdateController extends HttpServlet {
+
+    private static final String ERROR = "error.jsp",
+            SEARCH = "EquipmentManager.SearchController";
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        String url = ERROR;
+        try {
+            String code = request.getParameter("txtCode");
+            String username = request.getParameter("txtUsername");
+            String name = request.getParameter("txtName");
+            String type = request.getParameter("txtType");
+            String description = request.getParameter("txtDescription");
+
+            EquipmentDTO dto = new EquipmentDTO(code, username, name, type, description);
+            if (new EquipmentDAO().updateEquipment(dto)) {
+                url = SEARCH;
+            } else {
+                request.setAttribute("ERROR", "Could not update information's equipment");
+            }
+        } catch (Exception e) {
+            log("Error at EquipmentManager.UpdateController", e);
+        } finally {
+            request.getRequestDispatcher(url).forward(request, response);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ src/java/Tho/Controllers/Admin/EquipmentManager/UploadImageController.java
@@ -0,0 +1,128 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tho.Controllers.Admin.EquipmentManager;
+
+import Tho.Models.EquipmentDAO;
+import java.io.File;
+import java.io.IOException;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.tomcat.util.http.fileupload.FileItem;
+import org.apache.tomcat.util.http.fileupload.FileItemFactory;
+import org.apache.tomcat.util.http.fileupload.disk.DiskFileItemFactory;
+import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
+import org.apache.tomcat.util.http.fileupload.servlet.ServletRequestContext;
+
+/**
+ *
+ * @author ThoDT
+ */
+public class UploadImageController extends HttpServlet {
+
+    private static final String ERROR = "error.jsp",
+            SEARCH = "EquipmentManager.SearchController";
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        String url = ERROR;
+        try {
+            boolean isMultiPart = ServletFileUpload.isMultipartContent(request);
+            if (isMultiPart) {
+                FileItemFactory factory = new DiskFileItemFactory();
+                ServletFileUpload upload = new ServletFileUpload(factory);
+                List items = upload.parseRequest(new ServletRequestContext(request));
+
+                Iterator iter = items.iterator();
+                Hashtable params = new Hashtable();
+                String fileName = null;
+                FileItem savedItem = null;
+                while (iter.hasNext()) {
+                    FileItem item = (FileItem) iter.next();
+                    if (item.isFormField()) {
+                        params.put(item.getFieldName(), item.getString());
+                    } else {
+                        savedItem = item;
+                    }
+                }
+
+                String code = (String) params.get("txtCode");
+                String search = (String) params.get("txtSearch");
+                if (savedItem != null) {
+                    String itemName = savedItem.getName();
+                    fileName = itemName.substring(itemName.indexOf("\\") + 1);
+                    String extension = fileName.substring(fileName.indexOf(".") + 1);
+                    String realPath = getServletContext().getRealPath("/") + "src\\img\\[Equipment]" + code + "." + extension;
+                    File savedFile = new File(realPath);
+                    savedItem.write(savedFile);
+                    if (new EquipmentDAO().updateImage(code, "src/img/[Equipment]" + code + "." + extension)) {
+                        url = SEARCH + "?txtSearch=" + search;
+                    } else {
+                        request.setAttribute("ERROR", "Upload avatar failed");
+                    }
+                }
+            }
+        } catch (Exception e) {
+            log("Error at EquipmentManager.UploadImageController", e);
+        } finally {
+            request.getRequestDispatcher(url).forward(request, response);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
